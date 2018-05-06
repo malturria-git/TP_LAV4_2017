35ce63e1f0f1b6cd6366a217126696dec65cd641
Max <m.alturria@hotmail.com>
2018-05-06 14:24:17 -0300

    Push 4: about me

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -28,6 +28,9 @@ export class MenuComponent implements OnInit {
       case 'AgilidadaMasListado':
           this.router.navigate(['/Juegos/AgilidadaMasListado']);
         break;
+        case 'Tipeo':
+          this.router.navigate(['/Juegos/Tipeo']);
+        break;
     }
   }
 
